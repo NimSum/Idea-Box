ccaaf70ac6802a03af09ad16a9f5122735ed83d3
Nimrod Garcia <nimm.garcia@gmail.com>
2019-02-12 17:18:34 -0700

    Initial commit
    Co-authored-by: De'Marcus <tkd024@gmail.com>
    Co-authored-by: Ebenezer <ebenezerjaymusic@gmail.com>

---
+++ main.js
@@ -0,0 +1,35 @@
+var searchInput = document.querySelector('#search');
+var titleInput = document.querySelector('#idea-title');
+var ideaInput = document.querySelector('#idea-body');
+var saveBtn = document.querySelector('#save-btn');
+var cardSection = document.querySelector('#card-section');
+
+saveBtn.addEventListener('click', createCard);
+cardSection.addEventListener('click', deleteCard);
+// titleInput.addEventListener('keydown', function);
+// ideaInput.addEventListener('keydown', function);
+
+function createCard(event){
+  var ideaTitle = titleInput.value;
+  var ideaBody = ideaInput.value;
+
+  cardSection.innerHTML = 
+  `<article class="rounded-edges">
+      <h2>${ideaTitle}</h2>
+      <p class="lighter-font">${ideaBody}</p>
+      <div>
+        <a href="#" id="downvote-btn"><img src="images/downvote.svg" alt="downvote quality button"></a>
+        <a href="#" id="upvote-btn"><img src="images/upvote.svg" alt="upvote quality button"></a>
+        <h5>Quality: <span>Swill</span></h5>
+        <a href="" id="delete-btn"><img  class="delete-btn"src="images/delete.svg" alt="delete idea button"></a>
+      </div>
+    </article>` + cardSection.innerHTML;
+    event.preventDefault();
+}
+
+function deleteCard(event){
+   if(event.target.classList.contains('delete-btn')){
+    event.target.parentElement.parentElement.parentElement.remove();
+    event.preventDefault();
+  }
+}
